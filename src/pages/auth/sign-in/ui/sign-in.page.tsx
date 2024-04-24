ad0aebb8f19e1ba0955bddeb4582f3a4d586f4b1
EternalRival <eternalrival8@gmail.com>
2024-04-24 03:59:37 +0300

    feat: add 404 empty page

--- src/pages/auth/sign-in/ui/sign-in.page.tsx
+++ src/pages/auth/sign-in/ui/sign-in.page.tsx
@@ -1,5 +1,12 @@
+import { SignInForm } from '@/features/sign-in-form';
+
 import type { ReactNode } from 'react';
 
 export function SignInPage(): ReactNode {
-  return <h1>Sign-in Page</h1>;
+  return (
+    <>
+      <h1>Sign-in Page</h1>
+      <SignInForm />
+    </>
+  );
 }
