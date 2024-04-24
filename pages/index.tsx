fb7ef7aeb08bd29139e87219780710af2c97455d
EternalRival <eternalrival8@gmail.com>
2024-04-24 03:15:57 +0300

    feat: add empty pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,3 @@
-import { Inter } from 'next/font/google';
+import { MainPage } from '@/pages/main';
 
-import type { ReactNode } from 'react';
-
-const inter = Inter({ subsets: ['latin'] });
-
-export default function Home(): ReactNode {
-  return (
-    <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>Main</main>
-  );
-}
+export default MainPage;
---
+++ src/pages/auth/sign-up/ui/sign-up.page.tsx
@@ -0,0 +1,5 @@
+import type { ReactNode } from 'react';
+
+export function SignUpPage(): ReactNode {
+  return <h1>Sign-up Page</h1>;
+}
