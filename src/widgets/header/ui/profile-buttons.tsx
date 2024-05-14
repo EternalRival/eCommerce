1781f64db8b6aee14813c1182151f6cc74a78f74
EternalRival <eternalrival8@gmail.com>
2024-05-14 20:24:44 +0300

    feat: add spinners to profile buttons

--- src/widgets/header/ui/profile-buttons.tsx
+++ src/widgets/header/ui/profile-buttons.tsx
@@ -2,6 +2,7 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import LoginIcon from '@mui/icons-material/Login';
 import LogoutIcon from '@mui/icons-material/Logout';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import CircularProgress from '@mui/material/CircularProgress';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import { useRouter } from 'next/router';
@@ -11,11 +12,31 @@ import { useSignOut } from '~/features/auth';
 import { Route } from '~/shared/model/route.enum';
 
 import type { ReactNode } from 'react';
+import type { FCProps } from '~/shared/model/types';
+
+function ProfileButtonsSkeleton({ arrayLength }: FCProps<{ arrayLength: number }>): ReactNode {
+  return Array.from({ length: arrayLength }, (_, i) => (
+    <IconButton
+      key={i}
+      color="inherit"
+    >
+      <CircularProgress
+        size={24}
+        color="inherit"
+      />
+    </IconButton>
+  ));
+}
 
 export function ProfileButtons(): ReactNode {
-  const isCustomer = useAuthStore((store) => store.type === 'customer');
+  const auth = useAuthStore(({ type }) => ({ isPending: type === 'empty', isAnonymous: type === 'anonymous' }));
   const signOut = useSignOut();
   const router = useRouter();
+
+  if (auth.isPending) {
+    return <ProfileButtonsSkeleton arrayLength={2} />;
+  }
+
   const createNavigate = (route: Route) => (): void => void router.push(route);
 
   const customerButtonsProps = [
@@ -27,7 +48,7 @@ export function ProfileButtons(): ReactNode {
     ['Sign in', createNavigate(Route.AUTH_SIGN_IN), LoginIcon],
   ] as const;
 
-  return (isCustomer ? customerButtonsProps : anonymousButtonsProps).map(([title, onClick, Icon]) => (
+  return (auth.isAnonymous ? anonymousButtonsProps : customerButtonsProps).map(([title, onClick, Icon]) => (
     <Tooltip
       key={title}
       title={title}
