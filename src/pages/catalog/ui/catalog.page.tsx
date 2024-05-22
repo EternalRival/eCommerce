da11c321207c3fa22c274c02a0de077d23bd7128
EternalRival <eternalrival8@gmail.com>
2024-05-22 09:52:25 +0300

    refactor: remove unused gssp

--- src/pages/catalog/ui/catalog.page.tsx
+++ src/pages/catalog/ui/catalog.page.tsx
@@ -1,29 +1,9 @@
 import { Box } from '@mui/material';
 import Typography from '@mui/material/Typography';
 
-import type { GetServerSideProps } from 'next';
 import type { ReactNode } from 'react';
-import type { FCProps } from '~/shared/model/types';
 
-type Props = FCProps<{ data: number }>;
-
-export const getServerSideProps: GetServerSideProps<Props> = async () => {
-  const a = 1;
-
-  /*   const dehydratedState = createDehydratedState({ queryKey: ['catalog'], queryFn: getPagedQueryResult });
-
-  try {
-    const b = await getPagedQueryResult();
-
-    console.log(b);
-  } catch (error) {
-    console.log(error);
-  } */
-
-  return { props: { data: a } };
-};
-
-export function CatalogPage({ data }: Props): ReactNode {
+export function CatalogPage(): ReactNode {
   return (
     <>
       <Typography
@@ -38,7 +18,7 @@ export function CatalogPage({ data }: Props): ReactNode {
         component="pre"
         className="ring"
       >
-        {JSON.stringify(data, null, 2)}
+        123
       </Box>
     </>
   );
