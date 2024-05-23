36032d99170671113003afb7fb7b3187e6f83160
EternalRival <eternalrival8@gmail.com>
2024-05-23 23:04:53 +0300

    refactor: rename getCatalog to queryProductProjectionSearch

--- src/shared/api/commercetools/lib/index.ts
+++ src/shared/api/commercetools/lib/index.ts
@@ -1,8 +1,11 @@
 export { findCountryByLabel } from './find-country-by-label';
-export { getCatalog, type CatalogQueryResult } from './get-catalog';
 export { getTokenInfo } from './get-token-info';
 export { getTokenInfoByCredentials } from './get-token-info-by-credentials';
 export { isAllowedCountryName } from './is-allowed-country-name';
+export {
+  queryProductProjectionSearch,
+  type QueryProductProjectionSearchResult,
+} from './query-product-projection-search';
 export { signInCustomer } from './sign-in-customer';
 export { signUpCustomer } from './sign-up-customer';
 export { updateCustomer } from './update-customer';
