04a7c9d0b5cbb1ec44923cd902ce03c65176d276
EternalRival <eternalrival8@gmail.com>
2024-05-29 13:36:40 +0300

    feat: merge categories functions

--- src/pages/catalog/lib/index.ts
+++ src/pages/catalog/lib/index.ts
@@ -1,5 +1,4 @@
 export { createCategoriesBreadcrumbsProps } from './create-categories-breadcrumbs-props';
-export { getCurrentCategoryTree } from './get-current-category-tree';
 export { useCategoriesQuery } from './use-categories-query';
 export { useParseFilterOptions } from './use-parse-filter-options';
 export { usePruneInvalidCategoriesFromUrl } from './use-prune-invalid-categories-from-url';
