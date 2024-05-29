e201cc5481a656c483787ac4a93ddf757cd31457
EternalRival <eternalrival8@gmail.com>
2024-05-29 13:36:40 +0300

    feat: merge categories functions

--- src/pages/catalog/lib/create-categories-breadcrumbs-props.ts
+++ src/pages/catalog/lib/create-categories-breadcrumbs-props.ts
@@ -2,17 +2,31 @@ import type { BreadcrumbsLinkProps } from '~/entities/breadcrumbs';
 import type { Category } from '~/shared/api/commercetools';
 
 type Props = {
-  category?: Nullable<Category>;
+  categories: Category[];
+  slugList: string[];
   endpoint?: string;
 };
 
-export function createCategoriesBreadcrumbsProps({ category, endpoint = '' }: Props): BreadcrumbsLinkProps[] {
-  if (!category || !category.slug || !category.name) {
+export function createCategoriesBreadcrumbsProps({
+  categories,
+  slugList,
+  endpoint = '',
+}: Props): BreadcrumbsLinkProps[] {
+  const currentCategory = categories.find((category) => category.slug === slugList[0]);
+
+  if (!currentCategory || !currentCategory.slug || !currentCategory.name) {
     return [];
   }
 
-  const { id, name: label, slug } = category;
+  const { id, name: label, slug } = currentCategory;
   const href = `${endpoint}/${slug}`;
 
-  return [{ id, label, href }, ...createCategoriesBreadcrumbsProps({ category: category.children[0], endpoint: href })];
+  return [
+    { id, label, href },
+    ...createCategoriesBreadcrumbsProps({
+      categories: currentCategory.children,
+      slugList: slugList.slice(1),
+      endpoint: href,
+    }),
+  ];
 }
