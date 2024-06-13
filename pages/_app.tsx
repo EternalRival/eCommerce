96628512cc9816788cab0f120edb65ce5fe4f53c
EternalRival <eternalrival8@gmail.com>
2024-06-13 21:27:49 +0300

    build: update dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
 import Head from 'next/head';
 import { ErrorBoundary } from 'react-error-boundary';
-import 'react-toastify/dist/ReactToastify.min.css';
-import '~/_app/globals.css';
 
+import 'react-toastify/dist/ReactToastify.min.css';
 import { Layout, Providers } from '~/_app';
-import { siteTitle } from '~/shared/model/constants';
+import '~/_app/globals.css';
 import { InternalServerErrorPage } from '~/pages/internal-server-error';
+import { siteTitle } from '~/shared/model/constants';
 
 import type { AppProps } from 'next/app';
 import type { ReactNode } from 'react';
-import type { PageProps } from '~/_app/model';
+import type { PageProps } from '~/_app';
 
 export default function App<T>(appProps: AppProps<PageProps<T>>): ReactNode {
   const { Component, pageProps } = appProps;
