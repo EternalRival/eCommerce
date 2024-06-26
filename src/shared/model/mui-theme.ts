b43a3fd8d0e4acbef09029466dd15bf2eb47ff4e
EternalRival <eternalrival8@gmail.com>
2024-06-26 09:30:01 +0300

    feat: add modal for new addresses

--- src/shared/model/mui-theme.ts
+++ src/shared/model/mui-theme.ts
@@ -2,7 +2,31 @@ import { createTheme } from '@mui/material';
 
 import { roboto } from './next-fonts';
 
+const rootElement = typeof document === 'undefined' ? null : document.getElementById('__next');
+
 export const muiTheme = createTheme({
+  components: {
+    MuiModal: {
+      defaultProps: {
+        container: rootElement,
+      },
+    },
+    MuiDialog: {
+      defaultProps: {
+        container: rootElement,
+      },
+    },
+    MuiPopover: {
+      defaultProps: {
+        container: rootElement,
+      },
+    },
+    MuiPopper: {
+      defaultProps: {
+        container: rootElement,
+      },
+    },
+  },
   palette: {
     primary: {
       main: '#2e7d32',
