11e3ba57e84e4de5939ddf58da04c88a1fa25683
EternalRival <eternalrival8@gmail.com>
2024-07-20 01:25:04 +0300

    feat: implement delete address logic

--- src/widgets/customer-profile/lib/index.ts
+++ src/widgets/customer-profile/lib/index.ts
@@ -1,4 +1,5 @@
 export { createAddAddressActions } from './create-add-address-actions';
+export { createDeleteAddressActions } from './create-delete-address-actions';
 export { createSetAddressTypesActions } from './create-set-address-types-actions';
 export { createUpdatePersonalActions } from './create-update-personal-actions';
 export { useSyncAddressStateFactory } from './use-sync-address-state';
